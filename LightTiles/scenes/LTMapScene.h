55eacfb9c12914a11caa517ddfab766817d722b4
Kevin Jenkins <kevinj04@gmail.com>
2013-07-26 23:27:41 -0400

    add a basic scene for the map

---
+++ LightTiles/scenes/LTMapScene.h
@@ -0,0 +1,13 @@
+//
+//  LTMapScene.h
+//  LightTiles
+//
+//  Created by Kevin Jenkins on 7/26/13.
+//  Copyright (c) 2013 somethingpointless. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface LTMapScene : SKScene
+
+@end
--- LightTiles/scenes/LTMapScene.m
+++ LightTiles/scenes/LTMapScene.m
@@ -10,4 +10,23 @@
 
 @implementation LTMapScene
 
+#pragma mark - Initialization
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+
+        self.backgroundColor = [SKColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+    }
+    return self;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+
 @end
